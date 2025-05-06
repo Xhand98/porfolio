993b3a71bed11bc524fee105f532b0f5796e0036
Hendrick <hendrickherrera9@gmail.com>
2025-05-06 15:08:10 -0400

    Fixing skewprotect

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: { skewProtection: true },
+  adapter: vercel({
+    // Vercel adapter options
+    edgeMiddleware: true,
+    // Other Vercel-specific configurations if needed
+  }),
   integrations: [tailwind()],
   site: "https://xhand98.github.io/porfolio",
 });
