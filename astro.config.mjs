f7d745cf868b239a110780b210483b4344e16d5d
Hendrick <hendrickherrera9@gmail.com>
2025-05-06 17:56:08 -0400

    Fixing...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,9 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel"; // Remove "/serverless"
+import vercel from "@astrojs/vercel";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/vercel/image-service",
-    }),
-  ],
-  // Remove or update the site URL if deploying to Vercel
-  // site: "https://xhand98.github.io/porfolio",
+  integrations: [tailwind()],
 });
