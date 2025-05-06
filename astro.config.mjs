161908ddf7c389716582ec2d8beffeb45c575db4
Hendrick <hendrickherrera9@gmail.com>
2025-05-06 15:05:36 -0400

    Added skewProtection

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from "astro/config"
-
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: { skewProtection: true },
   integrations: [tailwind()],
   site: "https://xhand98.github.io/porfolio",
-})
+});
